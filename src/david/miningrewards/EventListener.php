55325dcebe5727ecf10d3153a946b16f713f286b
95CivicSi <aaron_salyer@yahoo.com>
2019-07-05 15:45:07 -0400

    EventListener: Add MONITOR priority to onBlockBreak to ensure that area protection plugins have a chance to cancel the event before MiningRewards gives players a reward.

--- src/david/miningrewards/EventListener.php
+++ src/david/miningrewards/EventListener.php
@@ -26,6 +26,7 @@ class EventListener implements Listener {
 
     /**
      * @param BlockBreakEvent $event
+     * @priority MONITOR
      */
     public function onBlockBreak(BlockBreakEvent $event) {
         if($event->isCancelled()) {
